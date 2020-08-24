c7e61906d83431e3ea50985c897090bfac88ae31
TitRadu <radutit@yahoo.com>
2020-08-25 02:36:38 +0300

    Creare proiect pentru parseCL.c + imbunatatiri parseCL.c(45min)

--- Auxiliar/parseCLL/parseCL.c
+++ Auxiliar/parseCLL/parseCL.c
@@ -7,17 +7,17 @@ void parse(wchar_t* path,char control,int* numberOfFiles,int* numberOfDirectorie
     WIN32_FIND_DATAW fileInfo;
     SYSTEMTIME fileLastWriteTime;
     LARGE_INTEGER fileSize;
-    wchar_t starPath[MAX_PATH];
+    wchar_t* starPath[MAX_PATH];
     starPath[0] = '\0';
-    wcscat_s(starPath,sizeof(starPath),path);
-    wcscat_s(starPath,sizeof(starPath),L"\\*");
-    wchar_t newPath[MAX_PATH];
+    strcat(starPath,path);
+    strcat(starPath,"\\*");
+    char newPath[1024];
     newPath[0] = '\0';
 
     float sizeMB = 0;
 
     DWORD error = 0;
-    if((hd = FindFirstFileW(starPath,&fileInfo)) == INVALID_HANDLE_VALUE){
+    if((hd = FindFirstFile(starPath,&fileInfo)) == INVALID_HANDLE_VALUE){
         error = GetLastError();
         if(error == ERROR_ACCESS_DENIED){
             (*numberOfDeniedFiles)++;
@@ -25,23 +25,23 @@ void parse(wchar_t* path,char control,int* numberOfFiles,int* numberOfDirectorie
             return;
 
         }
-        printf("Error:%lu\n",error);
+        printf("Error:%d\n",error);
         return;
 
 
     }
 
     do{
-        if((wcscmp(fileInfo.cFileName,L".") == 0) || (wcscmp(fileInfo.cFileName,L"..") == 0)){
+        if((strcmp(fileInfo.cFileName,".") == 0) || (strcmp(fileInfo.cFileName,"..") == 0)){
             continue;
 
         }
 
-        snwprintf(newPath,1024,L"%s\\%s",path,fileInfo.cFileName);
+        snprintf(newPath,1024,"%s\\%s",path,fileInfo.cFileName);
 
         if(FileTimeToSystemTime(&fileInfo.ftLastWriteTime,&fileLastWriteTime) == 0){
             error = GetLastError();
-            printf("Error:%lu\n",error);
+            printf("Error:%d\n",error);
 
         }
 
@@ -51,14 +51,14 @@ void parse(wchar_t* path,char control,int* numberOfFiles,int* numberOfDirectorie
 
         if(fileInfo.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY){
             (*numberOfDirectories)++;
-            wprintf(L"D\t%.3f\t\t%02d/%02d %02d:%02d:%02d %04d \t%s\n",sizeMB,fileLastWriteTime.wMonth,fileLastWriteTime.wDay,fileLastWriteTime.wHour,fileLastWriteTime.wMinute,fileLastWriteTime.wSecond,fileLastWriteTime.wYear,newPath);
+            printf("D\t%.3f\t\t%02d/%02d %02d:%02d:%02d %04d \t%s\n",sizeMB,fileLastWriteTime.wMonth,fileLastWriteTime.wDay,fileLastWriteTime.wHour,fileLastWriteTime.wMinute,fileLastWriteTime.wSecond,fileLastWriteTime.wYear,newPath);
             if(control == 'r' || control == 'R'){
                 parse(newPath,control,numberOfFiles,numberOfDirectories,numberOfDeniedFiles);
             }
             if(control == 'R'){
-                if(RemoveDirectoryW(newPath) == 0){
+                if(RemoveDirectory(newPath) == 0){
                     error = GetLastError();
-                    wprintf(L"Error:%lu---%s\n",error,newPath);
+                    printf("Error:%d---%s\n",error,newPath);
                     ExitProcess(1);
 
                 }
@@ -68,11 +68,11 @@ void parse(wchar_t* path,char control,int* numberOfFiles,int* numberOfDirectorie
             continue;
         }
         (*numberOfFiles)++;
-        wprintf(L"F\t%.3f\t\t%02d/%02d %02d:%02d:%02d %04d \t%s\n",sizeMB,fileLastWriteTime.wMonth,fileLastWriteTime.wDay,fileLastWriteTime.wHour,fileLastWriteTime.wMinute,fileLastWriteTime.wSecond,fileLastWriteTime.wYear,newPath);
+        printf("F\t%.3f\t\t%02d/%02d %02d:%02d:%02d %04d \t%s\n",sizeMB,fileLastWriteTime.wMonth,fileLastWriteTime.wDay,fileLastWriteTime.wHour,fileLastWriteTime.wMinute,fileLastWriteTime.wSecond,fileLastWriteTime.wYear,newPath);
         if(control == 'R'){
-            if(DeleteFileW(newPath) == 0){
+            if(DeleteFile(newPath) == 0){
                 error = GetLastError();
-                printf("Error:%lu\n",error);
+                printf("Error:%d\n");
                 ExitProcess(1);
 
             }
@@ -80,12 +80,12 @@ void parse(wchar_t* path,char control,int* numberOfFiles,int* numberOfDirectorie
         }
 
     }
-    while(FindNextFileW(hd,&fileInfo) != 0);
+    while(FindNextFile(hd,&fileInfo) != 0);
 
 
     if(FindClose(hd) == FALSE){
         error = GetLastError();
-        printf("FindCloseError:%lu\n",error);
+        printf("FindCloseError:%d\n",error);
         ExitProcess(error);
 
     }
@@ -96,21 +96,12 @@ void parse(wchar_t* path,char control,int* numberOfFiles,int* numberOfDirectorie
 
 int main(int argc,char* argv[]){
     SetConsoleCtrlHandler(NULL,FALSE);
-    LPWSTR commandLineString;
-    commandLineString = GetCommandLineW();
-    wchar_t separators[2] = L"\"";
-    wchar_t* part;
-    part = wcstok(commandLineString,separators);
-    part = wcstok(NULL,separators);
-    part = wcstok(NULL,separators);
-
     int numberOfFiles = 0;
     int numberOfDirectories = 0;
     int numberOfDeniedFiles = 0;
     printf("Type\tSize(MB)\tModified\t\t\tPath\n");
-    parse(part,argv[2][0],&numberOfFiles,&numberOfDirectories,&numberOfDeniedFiles);
+    parse(argv[1],argv[2][0],&numberOfFiles,&numberOfDirectories,&numberOfDeniedFiles);
     printf("Directory statistics: Files:%d   Directories:%d   DeniedFiles:%d\n",numberOfFiles,numberOfDirectories,numberOfDeniedFiles);
-
 
     return 0;
 }
